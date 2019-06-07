5e44afb6b29555d6fef1a2ce047a6c209cf3d325
Kossio <sergio@koss.io>
2019-06-07 20:09:40 +0900

    Reverted some changes/tests

--- scripts/summerofpride.js
+++ scripts/summerofpride.js
@@ -5,7 +5,7 @@ $(function(){
         var ds = new Miso.Dataset({
           importer : Miso.Dataset.Importers.GoogleSpreadsheet,
           parser : Miso.Dataset.Parsers.GoogleSpreadsheet,
-          key : "0AmiwpEaQwxurdDc5N0JQU1ZjQmdjeDVfWWJ6TEJuWHc",
+          key : "2PACX-1vTBySEbvEgVsSkmecl5SUDpQLEp5lzdPKWbGEPhILAE5w1YGnSwDo3TNHQ7ZoKGkiYfpijqS92tcOwQ",
           worksheet : "1"
         });
 
@@ -26,27 +26,30 @@ $(function(){
 
                 var tr = '<tr>';
 
-                row.lunes = nl2br(row.lunes);
-                row.martes = nl2br(row.martes);
-                row.miercoles = nl2br(row.miercoles);
-                row.jueves = nl2br(row.jueves);
-                row.viernes = nl2br(row.viernes);
-                row.sabado = nl2br(row.sabado);
+                row.saturday_streamer = nl2br(row.saturday_streamer);
+                row.sunday_streamer = nl2br(row.sunday_streamer);
+                row.monday_streamer = nl2br(row.monday_streamer);
+                row.tuesday_streamer = nl2br(row.tuesday_streamer);
+                row.wednesday_streamer = nl2br(row.wednesday_streamer);
+                row.thursday_streamer = nl2br(row.thursday_streamer);
+                row.friday_streamer = nl2br(row.friday_streamer);
 
-                row.instructor_lunes = nl2br(row.instructor_lunes);
-                row.instructor_martes = nl2br(row.instructor_martes);
-                row.instructor_miercoles = nl2br(row.instructor_miercoles);
-                row.instructor_jueves = nl2br(row.instructor_jueves);
-                row.instructor_viernes = nl2br(row.instructor_viernes);
-                row.instructor_sabado = nl2br(row.instructor_sabado);
+                row.saturday_game = nl2br(row.saturday_game);
+                row.sunday_game = nl2br(row.sunday_game);
+                row.monday_game = nl2br(row.monday_game);
+                row.tuesday_game = nl2br(row.tuesday_game);
+                row.wednesday_game = nl2br(row.wednesday_game);
+                row.thursday_game = nl2br(row.thursday_game);
+                row.friday_game = nl2br(row.friday_game);
 
-                  tr = tr+'<td nowrap="nowrap"><span class="horario">'+row.horario+'</span></td>';
-                  tr = tr+'<td nowrap="nowrap"><span class="name">'+row.lunes+'</span><span class="instructor">'+row.instructor_lunes+'</span></td>';
-                  tr = tr+'<td nowrap="nowrap"><span class="name">'+row.martes+'</span><span class="instructor">'+row.instructor_martes+'</span></td>';
-                  tr = tr+'<td nowrap="nowrap"><span class="name">'+row.miercoles+'</span><span class="instructor">'+row.instructor_miercoles+'</span></td>';
-                  tr = tr+'<td nowrap="nowrap"><span class="name">'+row.jueves+'</span><span class="instructor">'+row.instructor_jueves+'</span></td>';
-                  tr = tr+'<td nowrap="nowrap"><span class="name">'+row.viernes+'</span><span class="instructor">'+row.instructor_viernes+'</span></td>';
-                  tr = tr+'<td nowrap="nowrap"><span class="name">'+row.sabado+'</span><span class="instructor">'+row.instructor_sabado+'</span></td>';
+                  tr = tr+'<td nowrap="nowrap"><span class="horario">'+row.time-day+'</span></td>';
+                  tr = tr+'<td nowrap="nowrap"><span class="name">'+row.saturday_streamer+'</span><span class="instructor">'+row.saturday_game+'</span></td>';
+                  tr = tr+'<td nowrap="nowrap"><span class="name">'+row.sunday_streamer+'</span><span class="instructor">'+row.sunday_game+'</span></td>';
+                  tr = tr+'<td nowrap="nowrap"><span class="name">'+row.monday_streamer+'</span><span class="instructor">'+row.monday_game+'</span></td>';
+                  tr = tr+'<td nowrap="nowrap"><span class="name">'+row.tuesday_streamer+'</span><span class="instructor">'+row.tuesday_game+'</span></td>';
+                  tr = tr+'<td nowrap="nowrap"><span class="name">'+row.wednesday_streamer+'</span><span class="instructor">'+row.wednesday_game+'</span></td>';
+                  tr = tr+'<td nowrap="nowrap"><span class="name">'+row.thursday_streamer+'</span><span class="instructor">'+row.thursday_game+'</span></td>';
+                  tr = tr+'<td nowrap="nowrap"><span class="name">'+row.friday_streamer+'</span><span class="instructor">'+row.friday_game+'</span></td>';
 
                   $('#horario tbody').append(tr);
             });
