7e988ec5fcf0d260d99beb07344c049c29c15831
Kossio <sergio@koss.io>
2019-06-08 18:25:38 +0900

    Saturday Schedule. Completing week when we get more people confirmed :D

--- scripts/summerofpride.js
+++ scripts/summerofpride.js
@@ -68,7 +68,7 @@ $(function(){
               $('#div'+$(this).attr('target')).fadeIn("fast");
         });
     $('.targetDiv').fadeOut("fast");;
-$('#div1').fadeIn("fast");
+$('#div2').fadeIn("fast");
 
 });
 
